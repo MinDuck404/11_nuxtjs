f166abf14deb77ad3414588d132154fe2597a76d
MinDuck404 <duccjhin@gmail.com>
2025-08-18 14:51:19 +0700

    feat: Integrate Supabase client for backend functionality

---
+++ src/plugins/supabase.client.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+import { defineNuxtPlugin } from 'nuxt/app'
+
+const supabaseUrl = process.env.NUXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+export default defineNuxtPlugin(() => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: { persistSession: false }
+  })
+  return {
+    provide: {
+      supabase
+    }
+  }
+})
