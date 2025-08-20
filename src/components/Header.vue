dbfe20e59cc3ecc5cbf7aee64beb348c3f63ee8a
MinDuck404 <duccjhin@gmail.com>
2025-08-20 16:06:18 +0700

    navbar api

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,46 +5,27 @@
       <div class="row gx-lg-8 gx-xl-12 gy-10 gy-xl-0 mb-14 align-items-center">
         <div class="col-lg-7 order-lg-2">
           <figure v-if="heroItem">
-            <img
-              class="img-auto"
-              :src="heroItem.image_url"
-              :srcset="heroItem.image_url + ' 2x'"
-              :alt="heroItem.title"
-            />
+            <img class="img-auto" :src="heroItem.image_url" :srcset="heroItem.image_url + ' 2x'"
+              :alt="heroItem.title" />
           </figure>
           <figure v-else>
-            <img
-              class="img-auto"
-              src="/img/i21.png"
-              srcset="/img/i21.png 2x"
-              alt="Illustration"
-            />
+            <img class="img-auto" src="/img/i21.png" srcset="/img/i21.png 2x" alt="Illustration" />
           </figure>
         </div>
         <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-5 text-center text-lg-start">
-          <h1 v-if="heroSection" class="display-1 fs-54 mb-5 mx-md-n5 mx-lg-0 mt-7">
-            {{ heroSection.title }}
-            <br class="d-md-none" />
-            <span class="rotator-fade text-primary">
-              <span class="animate__animated animate__fadeInDown" style="display: inline-block;">
-                {{ heroSection.subtitle?.replace(/\u2026$/, '') || 'mobile design' }}
-              </span>
-            </span>
-          </h1>
-          <h1 v-else class="display-1 fs-54 mb-5 mx-md-n5 mx-lg-0 mt-7">
+
+          <h1 class="display-1 fs-54 mb-5 mx-md-n5 mx-lg-0 mt-7">
             A digital agency
             <br class="d-md-none" />specializing on
             <br class="d-md-none" />
             <span class="rotator-fade text-primary">
-              <span class="animate__animated animate__fadeInDown" style="display: inline-block;">
+              <span id="rotating-text" class="animate__animated animate__fadeInDown" style="display: inline-block;">
                 mobile design
               </span>
             </span>
           </h1>
-          <p v-if="heroSection && heroSection.subtitle" class="lead fs-lg mb-7">
-            {{ heroSection.subtitle.replace(/\u2026$/, '') }}
-          </p>
-          <p v-else class="lead fs-lg mb-7">
+
+          <p class="lead fs-lg mb-7">
             We are an award winning design agency that strongly believes in the power of creative ideas.
           </p>
           <span>
@@ -58,24 +39,18 @@
       <p v-if="clientsSection" class="text-center mb-8">{{ clientsSection.title }}</p>
       <p v-else class="text-center mb-8">Trusted by over 2K+ clients across the world</p>
       <div
-        class="row row-cols-4 row-cols-md-4 row-cols-lg-7 row-cols-xl-7 gy-10 mb-2 d-flex align-items-center justify-content-center"
-      >
+        class="row row-cols-4 row-cols-md-4 row-cols-lg-7 row-cols-xl-7 gy-10 mb-2 d-flex align-items-center justify-content-center">
         <div class="col" v-for="(client, index) in clients" :key="index">
-          <img
-            class="img-fluid px-md-3 px-lg-0 px-xl-2 px-xxl-5"
-            :src="client.image"
-            :alt="client.name"
-          />
+          <img class="img-fluid px-md-3 px-lg-0 px-xl-2 px-xxl-5" :src="client.image" :alt="client.name" />
         </div>
       </div>
     </div>
     <div class="overflow-hidden">
       <div class="divider text-soft-primary mx-n2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100">
-          <path
-            fill="currentColor"
-            d="M1260,1.65c-60-5.07-119.82,2.47-179.83,10.13s-120,11.48-180,9.57-120-7.66-180-6.42c-60,1.63-120,11.21-180,16a1129.52,1129.52,0,0,1-180,0c-60-4.78-120-14.36-180-19.14S60,7,30,7H0v93H1440V30.89C1380.07,23.2,1319.93,6.15,1260,1.65Z"
-          ></path>
+          <path fill="currentColor"
+            d="M1260,1.65c-60-5.07-119.82,2.47-179.83,10.13s-120,11.48-180,9.57-120-7.66-180-6.42c-60,1.63-120,11.21-180,16a1129.52,1129.52,0,0,1-180,0c-60-4.78-120-14.36-180-19.14S60,7,30,7H0v93H1440V30.89C1380.07,23.2,1319.93,6.15,1260,1.65Z">
+          </path>
         </svg>
       </div>
     </div>
@@ -143,6 +118,27 @@ onMounted(() => {
 onUnmounted(() => {
   itemStore.resetState();
 });
+
+  // Texts to rotate
+  onMounted(() => {
+  const texts = ["mobile design", "web design", "3D animation"];
+  let index = 0;
+  const el = document.getElementById("rotating-text");
+
+  function rotateText() {
+    el.classList.remove("animate__fadeInDown");
+    el.classList.add("animate__fadeOutUp");
+
+    setTimeout(() => {
+      index = (index + 1) % texts.length;
+      el.textContent = texts[index];
+      el.classList.remove("animate__fadeOutUp");
+      el.classList.add("animate__fadeInDown");
+    }, 500);
+  }
+
+  setInterval(rotateText, 3000);
+});
 </script>
 
 <style scoped>
@@ -152,7 +148,8 @@ onUnmounted(() => {
 
 .btn-primary:hover {
   transform: translateY(-2px);
-  background-color: #0056b3; /* Màu đậm hơn khi hover */
+  background-color: #0056b3;
+  /* Màu đậm hơn khi hover */
 }
 
 img.img-fluid {
@@ -160,11 +157,13 @@ img.img-fluid {
 }
 
 img.img-fluid:hover {
-  transform: scale(1.05); /* Phóng to nhẹ khi hover */
+  transform: scale(1.05);
+  /* Phóng to nhẹ khi hover */
   opacity: 0.9;
 }
 
 .rotator-fade .animate__animated {
-  animation-duration: 0.8s; /* Tùy chỉnh thời gian animation */
+  animation-duration: 0.8s;
+  /* Tùy chỉnh thời gian animation */
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,170 @@
+<!-- Navbar.vue -->
 <template>
-  
-    <nav class="navbar navbar-expand-lg classic transparent navbar-light navbar-clone fixed navbar-unstick">
+  <nav class="navbar navbar-expand-lg classic transparent navbar-light">
     <div class="container flex-lg-row flex-nowrap align-items-center">
       <div class="navbar-brand w-100">
-        <a href="https://sandbox.elemisthemes.com/index.html">
-          <img src="../assets/img/logo-dark.png" alt="">
+        <a href="#">
+          <img src="/images/logo-dark.png" srcset="/images/logo-dark@2x.png 2x" alt="">
         </a>
       </div>
       <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
+        <div class="offcanvas-header d-lg-none">
+          <h3 class="text-white fs-30 mb-0">Sandbox</h3>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
         <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Demos</a>
+            <!-- Demos Section (Grid Layout) -->
+            <li class="nav-item dropdown dropdown-mega" v-if="demos.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Demos</a>
+              <ul class="dropdown-menu mega-menu mega-menu-dark mega-menu-img">
+                <li class="mega-menu-content mega-menu-scroll">
+                  <ul class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-4 gy-lg-2 list-unstyled">
+                    <li class="col" v-for="demo in demos" :key="demo.id">
+                      <a class="dropdown-item" :href="demo.link_url || '#'">
+                        <figure class="rounded lift d-none d-lg-block">
+                          <img :src="demo.image_url" :srcset="demo.image_url + ' 2x'" :alt="demo.description">
+                        </figure>
+                        <span class="d-lg-none">{{ demo.description || `Demo ${demo.id}` }}</span>
+                      </a>
+                    </li>
+                  </ul>
+                  <span class="d-none d-lg-flex">
+                    <i class="uil uil-direction"></i><strong>Scroll to view more</strong>
+                  </span>
+                </li>
+              </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pages</a>
+
+            <!-- Pages Section (Nested Dropdowns) -->
+            <li class="nav-item dropdown" v-if="pages.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Pages</a>
+              <ul class="dropdown-menu">
+                <template v-for="pageGroup in pageGroups" :key="pageGroup.description">
+                  <li class="dropdown dropdown-submenu dropend">
+                    <a class="dropdown-item dropdown-toggle" href="#" data-bs-toggle="dropdown">{{ pageGroup.title }}</a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item" v-for="item in pageGroup.items" :key="item.id">
+                        <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                      </li>
+                    </ul>
+                  </li>
+                </template>
+              </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Projects</a>
+
+            <!-- Projects Section (Two-Column Layout) -->
+            <li class="nav-item dropdown" v-if="projects.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Projects</a>
+              <div class="dropdown-menu dropdown-lg">
+                <div class="dropdown-lg-content">
+                  <div>
+                    <h6 class="dropdown-header">Project Pages</h6>
+                    <ul class="list-unstyled">
+                      <li v-for="item in projectPages" :key="item.id">
+                        <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h6 class="dropdown-header">Single Projects</h6>
+                    <ul class="list-unstyled">
+                      <li v-for="item in singleProjects" :key="item.id">
+                        <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Blog</a>
+
+            <!-- Blog Section (Flat and Nested Items) -->
+            <li class="nav-item dropdown" v-if="blogs.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Blog</a>
+              <ul class="dropdown-menu">
+                <li class="nav-item" v-for="blog in blogs" :key="blog.id">
+                  <a class="dropdown-item" :href="blog.link_url || '#'">{{ blog.position || blog.description || blog.title }}</a>
+                </li>
+                <li class="dropdown dropdown-submenu dropend" v-if="blogPosts.length > 0">
+                  <a class="dropdown-item dropdown-toggle" href="#" data-bs-toggle="dropdown">Blog Posts</a>
+                  <ul class="dropdown-menu">
+                    <li class="nav-item" v-for="post in blogPosts" :key="post.id">
+                      <a class="dropdown-item" :href="post.link_url || '#'">{{ post.position || post.description || post.title }}</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Blocks</a>
+
+            <!-- Blocks Section (Grid with SVG Icons) -->
+            <li class="nav-item dropdown dropdown-mega" v-if="blocks.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Blocks</a>
+              <ul class="dropdown-menu mega-menu mega-menu-dark mega-menu-img">
+                <li class="mega-menu-content">
+                  <ul class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-6 gy-lg-4 list-unstyled">
+                    <li class="col" v-for="block in blocks" :key="block.id">
+                      <a class="dropdown-item" :href="block.link_url || '#'">
+                        <div class="rounded img-svg d-none d-lg-block p-4 mb-lg-2">
+                          <img class="rounded-0" :src="block.image_url || '/images/block1.svg'" :alt="block.description || block.title">
+                
+                          
+                        </div>
+                        <span>{{ block.description || block.title }}</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Documentation</a>
+
+            <!-- Documentation Section (Two-Column Layout) -->
+            <li class="nav-item dropdown dropdown-mega" v-if="documentation.length > 0">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Documentation</a>
+              <ul class="dropdown-menu mega-menu">
+                <li class="mega-menu-content">
+                  <div class="row gx-0 gx-lg-3">
+                    <div class="col-lg-4">
+                      <h6 class="dropdown-header">Usage</h6>
+                      <ul class="list-unstyled cc-2 pb-lg-1">
+                        <li v-for="item in usageItems" :key="item.id">
+                          <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                        </li>
+                      </ul>
+                      <h6 class="dropdown-header mt-lg-6">Styleguide</h6>
+                      <ul class="list-unstyled cc-2">
+                        <li v-for="item in styleguideItems" :key="item.id">
+                          <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-8">
+                      <h6 class="dropdown-header">Elements</h6>
+                      <ul class="list-unstyled cc-3">
+                        <li v-for="item in elementsItems" :key="item.id">
+                          <a class="dropdown-item" :href="item.link_url || '#'">{{ item.position || item.description || item.title }}</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </li>
           </ul>
           <!-- /.navbar-nav -->
+          <div class="offcanvas-footer d-lg-none">
+            <div>
+              <a href="mailto:first.last@email.com" class="link-inverse">info@email.com</a>
+              <br> 00 (123) 456 78 90 <br>
+              <nav class="nav social social-white mt-4">
+                <a href="#"><i class="uil uil-twitter"></i></a>
+                <a href="#"><i class="uil uil-facebook-f"></i></a>
+                <a href="#"><i class="uil uil-dribbble"></i></a>
+                <a href="#"><i class="uil uil-instagram"></i></a>
+                <a href="#"><i class="uil uil-youtube"></i></a>
+              </nav>
+            </div>
+          </div>
+          <!-- /.offcanvas-footer -->
         </div>
         <!-- /.offcanvas-body -->
       </div>
@@ -37,7 +172,7 @@
       <div class="navbar-other ms-lg-4">
         <ul class="navbar-nav flex-row align-items-center ms-auto">
           <li class="nav-item d-none d-md-block">
-            <a href="#" class="btn btn-sm btn-primary rounded-pill">Contact</a>
+            <a href="./contact.html" class="btn btn-sm btn-primary rounded-pill">Contact</a>
           </li>
           <li class="nav-item d-lg-none">
             <button class="hamburger offcanvas-nav-btn"><span></span></button>
@@ -49,90 +184,92 @@
     </div>
     <!-- /.container -->
   </nav>
-  <div class="content-wrapper">
-    <header class="wrapper bg-light">
-      <nav class="navbar navbar-expand-lg classic transparent navbar-light">
-        <div class="container flex-lg-row flex-nowrap align-items-center">
-          <div class="navbar-brand w-100">
-            <a href="https://sandbox.elemisthemes.com/index.html">
-              <img src="../assets/img/logo-dark.png"
-                srcset="../assets/img/logo-dark.png 2x" alt="">
-            </a>
-          </div>
-          <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
-            <div class="offcanvas-header d-lg-none">
-              <h3 class="text-white fs-30 mb-0">Sandbox</h3>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Demos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pages</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Projects</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Blocks</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Documentation</a>
-                </li>
-              </ul>
-              <!-- /.navbar-nav -->
-              <div class="offcanvas-footer d-lg-none">
-                <div>
-                  <a href="mailto:first.last@email.com" class="link-inverse">info@email.com</a>
-                  <br> 00 (123) 456 78 90 <br>
-                  <nav class="nav social social-white mt-4">
-                    <a href="#"><i class="uil uil-twitter"></i></a>
-                    <a href="#"><i class="uil uil-facebook-f"></i></a>
-                    <a href="#"><i class="uil uil-dribbble"></i></a>
-                    <a href="#"><i class="uil uil-instagram"></i></a>
-                    <a href="#"><i class="uil uil-youtube"></i></a>
-                  </nav>
-                  <!-- /.social -->
-                </div>
-              </div>
-              <!-- /.offcanvas-footer -->
-            </div>
-            <!-- /.offcanvas-body -->
-          </div>
-          <!-- /.navbar-collapse -->
-          <div class="navbar-other ms-lg-4">
-            <ul class="navbar-nav flex-row align-items-center ms-auto">
-              <li class="nav-item d-none d-md-block">
-                <a href="#" class="btn btn-sm btn-primary rounded-pill">Contact</a>
-              </li>
-              <li class="nav-item d-lg-none">
-                <button class="hamburger offcanvas-nav-btn"><span></span></button>
-              </li>
-            </ul>
-            <!-- /.navbar-nav -->
-          </div>
-          <!-- /.navbar-other -->
-        </div>
-        <!-- /.container -->
-      </nav>
-      <!-- /.navbar -->
-    </header>
-    <!-- /header -->
-  </div>
-
-  
 </template>
 
-<script>
-import { RouterLink } from 'vue-router'
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useItemStore } from '@/stores/useItemStore';
 
-export default {
-  name: 'Navbar'
-}
+// Store instance
+const itemStore = useItemStore();
+
+// State for navigation items
+const navItems = ref([]);
+const isLoadingNav = ref(false);
+const hasErrorNav = ref(false);
+const errorNav = ref(null);
+
+// Computed properties to separate items by category
+const demos = computed(() => navItems.value.filter(item => item.title === 'Demos'));
+const pages = computed(() => navItems.value.filter(item => ['Pages', 'Services', 'About', 'Shop', 'Contact', 'Career', 'Utility'].includes(item.title)));
+const projects = computed(() => navItems.value.filter(item => item.title === 'Projects'));
+const blogs = computed(() => navItems.value.filter(item => item.title === 'Blog' && !item.description?.includes('Posts')));
+const blogPosts = computed(() => navItems.value.filter(item => item.title === 'Blog' && item.description?.includes('Posts')));
+const blocks = computed(() => navItems.value.filter(item => ['About', 'Blog', 'Call to Action', 'Clients', 'Contact', 'Facts', 'FAQ', 'Features', 'Footer', 'Hero', 'Misc', 'Navbar', 'Portfolio', 'Pricing', 'Process', 'Team', 'Testimonials'].includes(item.title)));
+const documentation = computed(() => navItems.value.filter(item => item.title === 'Documentation' || item.title === 'Elements'));
+
+// Pages grouped by category
+const pageGroups = computed(() => {
+  const groups = {};
+  pages.value.forEach(item => {
+    const groupTitle = item.title;
+    if (!groups[groupTitle]) groups[groupTitle] = { title: groupTitle, items: [] };
+    groups[groupTitle].items.push(item);
+  });
+  return Object.values(groups).map(group => ({
+    title: group.title === 'Pages' ? group.items[0].description || group.title : group.title,
+    items: group.items
+  }));
+});
+
+// Projects split into Project Pages and Single Projects
+const projectPages = computed(() => projects.value.filter(item => item.description === 'Project Pages'));
+const singleProjects = computed(() => projects.value.filter(item => item.description === 'Single Projects'));
+
+// Documentation split into Usage, Styleguide, and Elements
+const usageItems = computed(() => documentation.value.filter(item => item.position === 'Get Started' || item.description === 'Usage'));
+const styleguideItems = computed(() => documentation.value.filter(item => ['Colors', 'Fonts', 'Icons SVG', 'Icons Font', 'Illustrations', 'Backgrounds', 'Misc'].some(desc => item.description?.includes(desc))));
+const elementsItems = computed(() => documentation.value.filter(item => item.title === 'Elements'));
+
+// Fetch navigation items
+const fetchNavItems = async () => {
+  try {
+    isLoadingNav.value = true;
+    const data = await itemStore.fetchItems('homepage_items', {
+      filters: { section_id: 1 },
+      orderBy: 'order_index',
+      ascending: true,
+      onlyReturn: true
+    });
+    if (data) {
+      navItems.value = data;
+    } else {
+      hasErrorNav.value = true;
+      errorNav.value = itemStore.error || 'Failed to load navigation items';
+    }
+  } catch (error) {
+    hasErrorNav.value = true;
+    errorNav.value = error.message || 'An error occurred';
+  } finally {
+    isLoadingNav.value = false;
+  }
+};
+
+// Initialize on component mount
+onMounted(() => {
+  fetchNavItems();
+});
+
+
 </script>
+
+<style scoped>
+.dropdown-item img {
+  height: 100px;
+  object-fit: cover;
+}
+.img-svg img {
+  width: 50px;
+  height: 50px;
+}
+</style>
